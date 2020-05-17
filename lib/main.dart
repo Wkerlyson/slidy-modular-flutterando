e7d414b8f3959189a8fa3a066c6faea4a4e23f6b
Wkerlyson Batista <wkerlyson.batista@uece.br>
2020-05-17 15:51:53 -0300

    class # 4: Automating with Slidy CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:slidy_modular_flutterando/app/app_module.dart';
-import 'package:slidy_modular_flutterando/app/app_widget.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(
-      module: AppModule(),
-    ));
+void main() => runApp(ModularApp(module: AppModule()));
